b801d5a41435b67b45069fb34322635f76c231ee
Paul FEVRE <git@paul-fevre.com>
2018-03-03 01:33:40 +0100

    [View] GitHub user search

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+// External imports
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
     moduleId: module.id,
@@ -6,6 +9,30 @@ import { Component } from '@angular/core';
     templateUrl: 'hello-world.component.html',
     styleUrls: ['hello-world.component.scss']
 })
-export class HelloWorldComponent {
-    title = 'Angular Bootstrapper';
+export class HelloWorldComponent implements OnInit {
+    public title = 'Angular Bootstrapper';
+    public user: any;
+    public formGroup: FormGroup;
+    public loading = false;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private httpClient: HttpClient
+    ) {}
+
+    ngOnInit() {
+        this.formGroup = this.formBuilder.group({
+            search: ['octocat']
+        });
+        this.fetchUser();
+    }
+
+    public fetchUser() {
+        this.loading = true;
+        const userId = this.formGroup.controls['search'].value;
+        this.httpClient.get(`https://api.github.com/users/${userId}`).toPromise().then(user => {
+            this.user = user;
+            this.loading = false;
+        });
+    }
 }
