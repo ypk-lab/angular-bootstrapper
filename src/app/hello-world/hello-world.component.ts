0ebab516c7323b5da7ab3e160b9b94ded20e3886
Paul FEVRE <git@paul-fevre.com>
2018-03-06 07:25:56 +0100

    [Service] Add user not found catch

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -14,6 +14,14 @@ export class HelloWorldComponent implements OnInit {
     public user: any;
     public formGroup: FormGroup;
     public loading = false;
+    public found = false;
+
+    public userNotFound = {
+        avatar_url: './assets/img/user-not-found.svg',
+        login: '?',
+        name: 'User not found',
+        html_url: ''
+    };
 
     constructor(
         private formBuilder: FormBuilder,
@@ -29,9 +37,15 @@ export class HelloWorldComponent implements OnInit {
 
     public fetchUser() {
         this.loading = true;
+        this.found = false;
         const userId = this.formGroup.controls['search'].value;
         this.httpClient.get(`https://api.github.com/users/${userId}`).toPromise().then(user => {
             this.user = user;
+            this.found = !!user;
+            this.loading = false;
+        }).catch(() => {
+            this.user = this.userNotFound;
+            this.found = false;
             this.loading = false;
         });
     }
