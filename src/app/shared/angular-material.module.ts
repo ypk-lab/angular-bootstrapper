892d93c1b8a9f88dbd4240587fdf7d3b06b7cfbd
Paul FEVRE <git@paul-fevre.com>
2020-07-22 10:03:15 +0200

    feat: (dependencies) update Angular Material to v8.2.3

--- src/app/shared/angular-material.module.ts
+++ src/app/shared/angular-material.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
-import {
-    MatButtonModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatProgressSpinnerModule,
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 const MODULES = [
     MatButtonModule,
